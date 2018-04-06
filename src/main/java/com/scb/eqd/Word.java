8b4d092d7475961614e5158852666d6daa6737f4
nishikant <nishi@nishikants-MacBook-Pro.local>
2018-04-07 07:03:26 +0800

    test

---
+++ src/main/java/com/scb/eqd/Word.java
@@ -0,0 +1,23 @@
+package com.scb.eqd;
+
+/**
+ * Created by nishi
+ */
+public class Word {
+    private String word;
+
+    public Word() {
+    }
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
